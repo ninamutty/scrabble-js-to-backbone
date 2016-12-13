e21c99e6919be95babd92fb8ba7c93df9f8c7f08
Chris <chris@adadevelopersacademy.org>
2016-12-13 12:18:44 -0800

    tested Constructor.

--- spec/player_spec.js
+++ spec/player_spec.js
@@ -5,11 +5,15 @@ import Player from 'player';
 describe('Player', function() {
   var player;
   beforeEach(function() {
-    player = new Player();
+    player = new Player("bob");
   });
   describe('Constructor', function() {
     it('Constructor Exists', function() {
       expect(Player).toBeFunction();
-  });
+    });
+    it('Constructor Initializes attributes', function() {
+      expect(player.name).toEqual("bob");
+      expect(player.plays).toEqual([]);
+    });
   });
 });
